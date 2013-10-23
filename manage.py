7594e8cd5fc0988e56dcb4f86b1c5c843d6bc8c9
Erik Chen <erikchen@erik-VirtualBox.(none)>
2013-10-22 21:00:32 -0700

    Update filters in views

--- manage.py
+++ manage.py
@@ -5,6 +5,6 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "real_OC.settings")
 
-    from django.core.management import execute_from_command_line
+    from django.core.management import *
 
     execute_from_command_line(sys.argv)
