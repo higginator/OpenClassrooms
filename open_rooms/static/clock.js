f89ce5d659b2bbcde00e6ed2c8eb6d8b349f4185
Ryan Higgins <ryanhigs@gmail.com>
2014-01-30 15:18:22 -0800

    functional clock mapping to a room's hours! Major breakthrough

--- open_rooms/static/clock.js
+++ open_rooms/static/clock.js
@@ -13,7 +13,9 @@ $(document).ready(function() {
 	var color;
 	var endAngle;
 	var oldEndAngle;
+	var times;
 	for (var i=0; i< canviis.length; i++) {
+		times = [3, 330, 4, 430, 5, 530, 6, 630, 7, 730, 8, 830, 9, 930, 10, 1030, 11, 1130, 12, 1230, 1, 130, 2, 230];
 		oldEndAngle = 0;
 		var ctx = canviis[i].getContext("2d");
 		ctx.font = "bold 16px sans-serif";
@@ -26,12 +28,27 @@ $(document).ready(function() {
 		//grab the list items that represent that rooms times
 		//and draw an appropriate clock
 		//arc (x-center, y-center, radius, startAngle, endAngle, counterClockwise)
-		for (var j=0; j < 25; j++) {
+		var building_name = $(canviis[i]).attr('name');
+		var items = $('ul[name*=' + building_name + '] > li');
+		for (var q=0; q < items.length; q++) {
+			var position = times.indexOf(parseInt($(items[q]).html()));
+			if (position > -1 ) {
+				//false meaning half-hour is occupied
+				times[position] = false;
+			};
+		};
+		for (var z=0; z < times.length; z++) {
+			if (typeof times[z] ==='number') {
+				times[z] = true;
+			};
+		};
+
+		for (var j=1; j < 25; j++) {
 			endAngle = j/12*Math.PI;
-			if (j % 2 == 0) {
-				color = 'red';
+			if (times[j-1]) {
+				color = '#32CD32';
 			} else {
-				color = 'green';
+				color = '#DC143C';
 			}
 			ctx.beginPath();
 			ctx.arc(start_x, start_y, clock_radius, oldEndAngle, endAngle, false);
@@ -44,4 +61,7 @@ $(document).ready(function() {
 			ctx.closePath();
 		};
 	};
+
+	$('#main_ul').remove();
+	$('#canvii_container').css('margin-top', '20px');
 });
