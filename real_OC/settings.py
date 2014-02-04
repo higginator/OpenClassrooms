cea9b5fd84c615251bad33e70d692d9692fce745
Ryan Higgins <ryanhigs@gmail.com>
2014-02-03 23:15:33 -0800

    a

--- real_OC/settings.py
+++ real_OC/settings.py
@@ -9,17 +9,17 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'open_rooms_db',                      # Or path to database file if using sqlite3.
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+   #     'NAME': 'open_rooms_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'Higginator',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}"""
+    #    'USER': 'Higginator',
+     #   'PASSWORD': '',
+      #  'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+       # 'PORT': '',                      # Set to empty string for default.
+    #}
+#}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
