8dc0fc4dd008651c4770b22bdc51e412dd772c4a
Ryan Higgins <ryanhigs@gmail.com>
2014-02-03 23:20:51 -0800

    orig

--- real_OC/settings.py
+++ real_OC/settings.py
@@ -162,13 +162,13 @@ LOGGING = {
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 DATABASES['default']['NAME'] = 'open_rooms_db'
 DATABASES['default']['USER'] = 'Higginator'
 DATABASES['default']['PASSWORD'] = ''
 DATABASES['default']['HOST'] = ''
 DATABASES['default']['PORT'] = ''
-DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
