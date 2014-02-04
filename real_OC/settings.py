6514c7f9cec54f51af827baff5b7b6da32e357f6
Ryan Higgins <ryanhigs@gmail.com>
2014-02-03 23:19:14 -0800

    af

--- real_OC/settings.py
+++ real_OC/settings.py
@@ -168,7 +168,7 @@ DATABASES['default']['USER'] = 'Higginator'
 DATABASES['default']['PASSWORD'] = ''
 DATABASES['default']['HOST'] = ''
 DATABASES['default']['PORT'] = ''
-#DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
